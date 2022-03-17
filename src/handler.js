20b17e94e173c9ec15da14a1c28659fda9894370
andremelinski <andremelinski29@gmail.com>
2022-03-17 13:50:41 -0300

    fixing function namer at yml

--- src/handler.js
+++ src/handler.js
@@ -4,7 +4,4 @@ module.exports.randomNumber = (event) => {
   const random = parseInt(Math.random() * 100);
   console.log(`generating random number: ${random}`);
   return random
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
