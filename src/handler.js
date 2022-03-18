8c02ca0b6965a3280de8b2e99303a70dee169dcc
andremelinski <andremelinski29@gmail.com>
2022-03-18 12:55:12 -0300

    creating s3 yml

--- src/handler.js
+++ src/handler.js
@@ -2,6 +2,6 @@
 
 module.exports.randomNumber = (event) => {
   const random = parseInt(Math.random() * 100);
-  console.log(`generating random number: ${random}`);
+  console.log(`generating random number in a new branch: ${random}`);
   return random
 };
