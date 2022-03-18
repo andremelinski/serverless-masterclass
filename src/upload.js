16bd40717cecabf01cb2be6f19ebfb2d08f8b9a7
andremelinski <andremelinski29@gmail.com>
2022-03-18 15:53:49 -0300

    adding plugin

--- src/upload.js
+++ src/upload.js
@@ -1,38 +1,37 @@
-"use strict";
-require("dotenv").config();
+
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
 
 module.exports.handler = async (event) => {
-  console.log(event);
+    console.log(event);
 
-  const response = {
-    isBase64Encoded: false,
-    statusCode: 200,
-    body: JSON.stringify({ message: "Successfully uploaded file to S3" }),
-  };
-
-  try {
-    const parsedBody = JSON.parse(event.body);
-    const base64File = parsedBody.file;
-    const decodedFile = Buffer.from(base64File.replace(/^data:image\/\w+;base64,/, ""), "base64");
-    const params = {
-      Bucket: BUCKET_NAME,
-      Key: `images/${new Date().toISOString()}.jpeg`,
-      Body: decodedFile,
-      ContentType: "image/jpeg",
+    const response = {
+        isBase64Encoded: false,
+        statusCode: 200,
+        body: JSON.stringify({ message: "Successfully uploaded file to S3" }),
     };
 
-    const uploadResult = await s3.upload(params).promise();
+    try {
+        const parsedBody = JSON.parse(event.body);
+        const base64File = parsedBody.file;
+        const decodedFile = Buffer.from(base64File.replace(/^data:image\/\w+;base64,/, ""), "base64");
+        const params = {
+            Bucket: BUCKET_NAME,
+            Key: `images/${new Date().toISOString()}.jpeg`,
+            Body: decodedFile,
+            ContentType: "image/jpeg",
+        };
 
-    response.body = JSON.stringify({ message: "Successfully uploaded file to S3", uploadResult });
-  } catch (e) {
-    console.error(e);
-    response.body = JSON.stringify({ message: "File failed to upload", errorMessage: e });
-    response.statusCode = 500;
-  }
+        const uploadResult = await s3.upload(params).promise();
 
-  return response;
+        response.body = JSON.stringify({ message: "Successfully uploaded file to S3", uploadResult });
+    } catch (e) {
+        console.error(e);
+        response.body = JSON.stringify({ message: "File failed to upload", errorMessage: e });
+        response.statusCode = 500;
+    }
+
+    return response;
 };
