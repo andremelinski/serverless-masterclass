5187d2641b6f4e23e368ae56ef093792bc0ef08c
andremelinski <andremelinski29@gmail.com>
2022-03-19 10:02:18 -0300

    adding new upload image logic

--- src/endpoints/imageUpload.js
+++ src/endpoints/imageUpload.js
@@ -1,16 +1,50 @@
-const Responses = require('../common/responses')
-const allowedMimes = ['image/jpeg', 'image/png', 'image/gif']
-exports.handler = async event =>{
-    try {
-        const body = JSON.parse(event.body)
+const Responses = require("../common/responses");
+const fileType = require("file-type");
+const uuid = require("uuid");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+require('dotenv').config()
 
-        if(!body || !body.image || !body.mime) return Responses._400({message: 'Incorrect body on request'})
+const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
 
-        if(!allowedMimes.includes(body.mime)) return Responses._400({message: 'Incorrect mime type'})
+const allowedMimes = ["image/jpeg", "image/png", "image/gif"];
+exports.handler = async (event) => {
+  try {
+    console.log(event);
+    const body = JSON.parse(event.body);
+    let { image: imageData = "" } = body;
 
-        return event
+    if (!body || !imageData || !body.mime) return Responses._400({ message: "Incorrect body on request" });
+    if (!allowedMimes.includes(body.mime)) return Responses._400({ message: "Incorrect mime type" });
 
-    } catch (error) {
-        return Responses._404({message: JSON.stringify(error,null,2)})
+    if (imageData.image.substr(0, 7) === "base64") {
+      imageData = imageData.substr(7, imageData.length);
     }
-}
+
+    const buffer = Buffer.from(imageData, "base64");
+    const fileInfo = await fileType.fromBuffer(buffer);
+
+    const detectedExt = fileInfo.ext;
+    const detectedMime = fileInfo.mime;
+
+    if (detectedMime !== body.mime) return Responses._400({ message: "detectedMime !== body.mime" });
+
+    const fileName = `images/${new Date().toISOString()}-${uuid.v5()}-${detectedExt}`;
+    const params = {
+        Bucket: BUCKET_NAME,
+        Key: `images/${fileName}`,
+        Body: buffer,
+        ContentType: body.mime,
+        ACL: 'public-read'
+    };
+
+    const s3File = await s3.putObject(params).promise();
+
+    // https://BUCKET_NAME.s3-${region}.amazonaws.com/images/${fileName}
+
+    return Responses._200({ image: s3File });
+    
+  } catch (error) {
+    return Responses._404({ message: JSON.stringify(error, null, 2) });
+  }
+};
