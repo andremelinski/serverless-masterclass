dee8563246be43ef8798836cfbb6b39d4312e1a4
andremelinski <andremelinski29@gmail.com>
2022-03-19 12:26:33 -0300

    adding changes

--- src/endpoints/imageUpload.js
+++ src/endpoints/imageUpload.js
@@ -1,21 +1,22 @@
-const Responses = require('../common/API_Responses');
-const fileType = require("file-type");
-const uuid = require("uuid");
-const AWS = require("aws-sdk");
+import ApiResponses from '../common/API_Responses.js';
+import * as fileType from 'file-type';
+import { v5 } from "uuid";
+import * as AWS from 'aws-sdk';
 const s3 = new AWS.S3();
-require('dotenv').config()
+import dotenv from 'dotenv'
+dotenv.config()
 
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
 
 const allowedMimes = ["image/jpeg", "image/png", "image/gif"];
-exports.handler = async (event) => {
+export async function handler(event) {
   try {
     console.log(event);
     const body = JSON.parse(event.body);
     let { image: imageData = "" } = body;
 
-    if (!body || !imageData || !body.mime) return Responses._400({ message: "Incorrect body on request" });
-    if (!allowedMimes.includes(body.mime)) return Responses._400({ message: "Incorrect mime type" });
+    if (!body || !imageData || !body.mime) return ApiResponses._400({ message: "Incorrect body on request" });
+    if (!allowedMimes.includes(body.mime)) return ApiResponses._400({ message: "Incorrect mime type" });
 
     if (imageData.image.substr(0, 7) === "base64") {
       imageData = imageData.substr(7, imageData.length);
@@ -27,9 +28,9 @@ exports.handler = async (event) => {
     const detectedExt = fileInfo.ext;
     const detectedMime = fileInfo.mime;
 
-    if (detectedMime !== body.mime) return Responses._400({ message: "detectedMime !== body.mime" });
+    if (detectedMime !== body.mime) return ApiResponses._400({ message: "detectedMime !== body.mime" });
 
-    const fileName = `images/${new Date().toISOString()}-${uuid.v5()}-${detectedExt}`;
+    const fileName = `images/${new Date().toISOString()}-${v5()}-${detectedExt}`;
     const params = {
         Bucket: BUCKET_NAME,
         Key: `images/${fileName}`,
@@ -42,9 +43,9 @@ exports.handler = async (event) => {
 
     // https://BUCKET_NAME.s3-${region}.amazonaws.com/images/${fileName}
 
-    return Responses._200({ image: s3File });
+    return ApiResponses._200({ image: JSON.stringify(s3File, null, 2) });
     
   } catch (error) {
-    return Responses._404({ message: JSON.stringify(error, null, 2) });
+    return ApiResponses._404({ message: JSON.stringify(error, null, 2) });
   }
-};
+}
