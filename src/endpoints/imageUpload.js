7a1ab7237b7ad85f2cbc461f392eff57c233be93
andremelinski <andremelinski29@gmail.com>
2022-03-20 12:14:01 -0300

    some changes

--- src/endpoints/imageUpload.js
+++ src/endpoints/imageUpload.js
@@ -1,58 +1,60 @@
-import ApiResponses from '../common/API_Responses.js';
-import * as fileType from 'file-type';
-import { v4 as uuid } from "uuid";
-// import * as AWS from 'aws-sdk';
-import AWS from 'aws-sdk';
-import dotenv from 'dotenv'
-dotenv.config()
+// const fileType = require('file-type');
+const ApiResponses = require('../common/API_Responses');
+const uuid = require('uuid').v4();
+const AWS = require('aws-sdk');
+require('dotenv').config()
 
-const { S3 } = AWS;
-const s3 = new S3();
-
+const s3 = new AWS.S3();
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
-const allowedMimes = ["image/jpeg", "image/png", "image/gif"];
-export async function handler(event) {
-  try {
-    console.log(BUCKET_NAME);
-    console.log(event)
-    if(!event.body) return ApiResponses._400({ message: event });
+const allowedMimes = ['image/jpeg', 'image/png', 'image/jpg'];
+exports.handler = async event => {
+    try {
+        
+        const body = JSON.parse(event.body);
 
-    const body = JSON.parse(event.body);
-    
-    if (!body || !imageData || !body.mime) return ApiResponses._400({ message: "Incorrect body on request" });
-    if (!allowedMimes.includes(body.mime)) return ApiResponses._400({ message: "Incorrect mime type" });
-    
-    let imageData = body.image;
-    if (imageData.image.substr(0, 7) === "base64") {
-        imageData = imageData.substr(7, imageData.length);
+        if (!body || !body.image || !body.mime) {
+            return ApiResponses._400({ message: 'incorrect body on request' });
+        }
+
+        if (!allowedMimes.includes(body.mime)) {
+            return ApiResponses._400({ message: 'mime is not allowed ' });
+        }
+
+        let imageData = body.image;
+        if (body.image.substr(0, 7) === 'base64,') {
+            imageData = body.image.substr(7, body.image.length);
+        }
+
+        const buffer = Buffer.from(imageData, 'base64');
+        // const fileInfo = await fileType.fromBuffer(buffer);
+        // const detectedExt = fileInfo.ext;
+        // const detectedMime = fileInfo.mime;
+
+        // if (detectedMime !== body.mime) {
+        //     return ApiResponses._400({ message: 'mime types dont match' });
+        // }
+
+        const key = `${uuid()}-${new Date().toISOString()}`;
+
+        console.log(`writing image to bucket called ${key}`);
+
+        const bla = await s3
+            .putObject({
+                Body: buffer,
+                Key: `images/${key}`,
+                ContentType: body.mime,
+                Bucket: BUCKET_NAME,
+                ACL: 'public-read',
+            })
+            .promise();
+
+        // const url = `https://${BUCKET_NAME}.s3-${process.env.region}.amazonaws.com/${key}`;
+        return ApiResponses._200({
+            imageURL: bla,
+        });
+    } catch (error) {
+        console.log('error', error);
+
+        return ApiResponses._404({ message: error.message, event: JSON.parse(event.body) });
     }
-    
-    return ApiResponses._200({ imageData, event, BUCKET_NAME });
-    const buffer = Buffer.from(imageData, "base64");
-    const fileInfo = await fileType.fromBuffer(buffer);
-
-    const detectedExt = fileInfo.ext;
-    const detectedMime = fileInfo.mime;
-    console.log({detectedExt, detectedMime});
-    if (detectedMime !== body.mime) return ApiResponses._400({ message: "detectedMime !== body.mime" });
-
-    const fileName = `${new Date().toISOString()}-${uuid()}-${detectedExt}`;
-    const params = {
-        Bucket: BUCKET_NAME,
-        Key: `images/${fileName}`,
-        Body: buffer,
-        ContentType: body.mime,
-        ACL: 'public-read'
-    };
-
-    const s3File = await s3.putObject(params).promise();
-    console.log(s3File)
-    // https://BUCKET_NAME.s3-${region}.amazonaws.com/images/${fileName}
-
-    return ApiResponses._200({ image: JSON.stringify(s3File, null, 2) });
-    
-  } catch (error) {
-      console.log(error);
-    return ApiResponses._404({ message: error.message });
-  }
-}
+};
