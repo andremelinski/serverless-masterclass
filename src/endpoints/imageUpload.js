bb266d04d2efefad8e3af90f065e2bb062bd87e5
andremelinski <andremelinski29@gmail.com>
2022-03-20 13:59:15 -0300

    some changes

--- src/endpoints/imageUpload.js
+++ src/endpoints/imageUpload.js
@@ -1,4 +1,4 @@
-// const fileType = require('file-type');
+const fileType = import('file-type');
 const ApiResponses = require('../common/API_Responses');
 const uuid = require('uuid').v4();
 const AWS = require('aws-sdk');
@@ -10,47 +10,32 @@ const allowedMimes = ['image/jpeg', 'image/png', 'image/jpg', 'image/jpeg'];
 exports.handler = async event => {
     try {
         
-        const body = JSON.parse(event.body);
+        const parsedBody = JSON.parse(event.body);
+        const base64File = parsedBody.file;
+        const decodedFile = Buffer.from(base64File.replace(/^data:image\/\w+;base64,/, ""), "base64");
 
-        if (!body || !body.image || !body.mime) {
-            return ApiResponses._400({ message: 'incorrect body on request' });
-        }
-
-        if (!allowedMimes.includes(body.mime)) {
-            return ApiResponses._400({ message: 'mime is not allowed ' });
-        }
-
-        let imageData = body.image;
-        if (body.image.substr(0, 7) === 'base64,') {
-            imageData = body.image.substr(7, body.image.length);
-        }
-
-        const buffer = Buffer.from(imageData, 'base64');
-        // const fileInfo = await fileType.fromBuffer(buffer);
-        // const detectedExt = fileInfo.ext;
-        // const detectedMime = fileInfo.mime;
-
+        const fileInfo = await fileType.fromBuffer(buffer);
+        const detectedExt = fileInfo.ext;
+        const detectedMime = fileInfo.mime;
         // if (detectedMime !== body.mime) {
         //     return ApiResponses._400({ message: 'mime types dont match' });
         // }
 
-        const key = `${uuid()}-${new Date().toISOString()}`;
+        const fileName = `${uuid()}-${new Date().toISOString()}-${decodedFile}-${detectedExt}-${detectedMime}`;
 
-        console.log(`writing image to bucket called ${key}`);
-
-        const bla = await s3
-            .putObject({
-                Body: buffer,
-                Key: `images/${key}`,
-                ContentType: body.mime,
-                Bucket: BUCKET_NAME,
-                ACL: 'public-read',
-            })
-            .promise();
+        // const bla = await s3
+        //     .putObject({
+        //         Body: decodedFile,
+        //         Key: `images/${fileName}`,
+        //         ContentType: body.mime,
+        //         Bucket: BUCKET_NAME,
+        //         ACL: 'public-read',
+        //     })
+        //     .promise();
 
         // const url = `https://${BUCKET_NAME}.s3-${process.env.region}.amazonaws.com/${key}`;
         return ApiResponses._200({
-            imageURL: bla,
+            imageURL:  fileName,
         });
     } catch (error) {
         console.log('error', error);
