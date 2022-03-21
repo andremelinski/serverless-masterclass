0d5e314f5c34f90701576b86a0f7ec40caed2113
andremelinski <andremelinski29@gmail.com>
2022-03-21 13:58:47 -0300

    adding CRUD

--- src/endpoints/getAllImages.js
+++ src/endpoints/getAllImages.js
@@ -1,14 +1,17 @@
 const Responses = require('../common/API_Responses')
 require("dotenv").config()
-const {v4 : uuid} = require("uuid");
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 
-const REGION = process.env.REGION || "us-east-1";
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
 
-const allowedMimes = ['image/jpeg', 'image/png', 'image/jpg'];
-const contentTypeRegex = /data:\s*(\w+\/\w+)/
 exports.handler = async (event) => {
-    return Responses._200({ uploadResult, url });
+
+    try{
+        const files = await s3.listObjectsV2({Bucket: BUCKET_NAME, Prefix: 'images'}).promise()
+        return Responses._200({ files });
+    } catch (error) {
+        console.log('error', error);
+        return Responses._400({ message: error.message || 'failed to upload image' });
+    }
 }
