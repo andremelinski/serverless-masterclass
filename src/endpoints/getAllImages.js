fed4bf645771dd709704bd4ab6a9b823853137df
andremelinski <andremelinski29@gmail.com>
2022-03-21 16:41:50 -0300

    adding querystring

--- src/endpoints/getAllImages.js
+++ src/endpoints/getAllImages.js
@@ -15,7 +15,7 @@ exports.handler = async (event) => {
 
         if(key){
             params.Key = `${prefix}/${key}`
-            const data = await s3.getObject(params).promise();
+            const data = await s3.listObjects(params).promise();
             return Responses._200({ data });
         }
 
