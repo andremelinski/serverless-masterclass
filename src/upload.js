01fd0d0d369d443bdadc231b3abe418bfe57c30e
andremelinski <andremelinski29@gmail.com>
2022-03-21 00:41:55 -0300

    adding changes

--- src/upload.js
+++ src/upload.js
@@ -1,8 +1,8 @@
+require("dotenv").config()
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
-const fileType = require('file-type');
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
-
+const contentTypeRegex = /(Content-Type:\s*\w+\/\w+)/
 module.exports.handler = async (event) => {
 
     const response = {
@@ -12,8 +12,9 @@ module.exports.handler = async (event) => {
 
     try {
         const parsedBody = Buffer.from(event.body, 'base64');
+        let contentType =  contentTypeRegex.exec(event.body)
+        contentType = contentType.split(':')[1].trim()
 
-    //     const decodedFile = Buffer.from(base64File.replace(/^data:image\/\w+;base64,/, ""), "base64");
     //     const params = {
     //         Bucket: BUCKET_NAME,
     //         Key: `images/${new Date().toISOString()}.jpeg`,
@@ -23,7 +24,7 @@ module.exports.handler = async (event) => {
 
     //     const uploadResult = await s3.upload(params).promise();
 
-        response.body = JSON.stringify({ message: "Successfully uploaded file to S3", parsedBody, event: event.body });
+        response.body = JSON.stringify({ message: "Successfully uploaded file to S3", parsedBody, contentType, BUCKET_NAME });
     } catch (e) {
         console.error(e);
         response.body = JSON.stringify({ message: "File failed to upload", errorMessage: e });
