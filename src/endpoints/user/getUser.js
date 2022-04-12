9b9ee8660a59f8db85b9068dc95cf47fe91f7fe0
andremelinski <andremelinski29@gmail.com>
2022-04-12 18:34:48 -0300

    add dynamo

--- src/endpoints/user/getUser.js
+++ src/endpoints/user/getUser.js
@@ -4,17 +4,17 @@ const Dynamo = require('../../common/DynamoDB')
 
 const userTableName = process.env.USER_TABLE_NAME
 exports.handler = async (event) => {
-
-    if(event.pathParameters || event.pathParameters.ID) return Responses._400({message: "Missing ID from the path"})
+    
+    if(event.pathParameters || event.pathParameters.ID) return Responses._400({message: event.pathParameters})
 
     let {ID} = event.pathParameters;
-
+    console.log({userTableName, ID})
     const user = await Dynamo.get(ID, userTableName).catch(err => {
         console.log("error in Dynamo Get",err)
         return null
     });
 
-    if(!user) return Responses._400({message: "Failed to get user by ID"})
+    if(!user) return Responses._400({message: {ID, userTableName}})
 
     return Responses._200({user})
 }
