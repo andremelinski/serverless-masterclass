8934a4bb05f79c161652d4d247b8a3b22351d7ae
andremelinski <andremelinski29@gmail.com>
2022-04-13 01:09:38 -0300

    add dynamo

--- src/endpoints/user/getPlayerScore.js
+++ src/endpoints/user/getPlayerScore.js
@@ -1,20 +1,21 @@
 require("dotenv").config()
 const Responses = require('../../common/API_Responses')
 const Dynamo = require('../../common/DynamoDB')
+const {dynamoError} = require('../common/helper/errorHandling')
 
 const USER_TABLE_NAME = process.env.USER_TABLE_NAME
 exports.handler = async (event) => {
+    
+    let {ID = ''} = event.pathParameters;
+    if(!event.pathParameters || !ID) return Responses._400({message: `${await dynamoError('noId', TableName)}`})
 
-    if(event.pathParameters || event.pathParameters.ID) return Responses._400({message: "Missing ID from the path"})
 
-    let {ID} = event.pathParameters;
-    console.log(`Table name ${USER_TABLE_NAME}, ID ${ID}`)
     const user = await Dynamo.get(ID, USER_TABLE_NAME).catch(err => {
         console.log("error in Dynamo Get",err)
         return null
     });
 
-    if(!user) return Responses._400({message: "Failed to get user by ID"})
+    if(!user) return Responses._400({message: `${await dynamoError('fetching', TableName, ID)}`})
 
     return Responses._200({user})
 }
