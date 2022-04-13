3230873cca81ebc63862191bc0eb4deb256739d0
andremelinski <andremelinski29@gmail.com>
2022-04-13 01:27:45 -0300

    error handling

--- src/endpoints/user/getUser.js
+++ src/endpoints/user/getUser.js
@@ -1,21 +1,31 @@
-require("dotenv").config()
-const Responses = require('../../common/API_Responses')
-const Dynamo = require('../../common/DynamoDB')
-const {dynamoError} = require('../../common/helper/errorHandling')
+require("dotenv").config();
+const Responses = require("../../common/API_Responses");
+const Dynamo = require("../../common/DynamoDB");
+const { dynamoError } = require("../../common/helper/errorHandling");
 
-const USER_TABLE_NAME = process.env.USER_TABLE_NAME
+const USER_TABLE_NAME = process.env.USER_TABLE_NAME;
 exports.handler = async (event) => {
-    
-    let {ID = ''} = event.pathParameters;
-    if(!event.pathParameters || !ID) return Responses._400({message: `${await dynamoError('noId', TableName)}`})
+  try {
+    let message = "";
+    let { ID = "" } = event.pathParameters;
 
+    if (!event.pathParameters || !ID) {
+      message = await dynamoError("noId", TableName);
+      return Responses._400({ message });
+    }
 
-    const user = await Dynamo.get(ID, USER_TABLE_NAME).catch(err => {
-        console.log("error in Dynamo Get",err)
-        return null
+    const user = await Dynamo.get(ID, USER_TABLE_NAME).catch((err) => {
+      console.log("error in Dynamo Get", err);
+      return null;
     });
 
-    if(!user) return Responses._400({message: `${await dynamoError('fetching', TableName, ID)}`})
+    if (!user) {
+      message = await dynamoError("fetching", TableName, ID);
+      return Responses._400({ message });
+    }
 
-    return Responses._200({user})
-}
+    return Responses._200({ user });
+  } catch (err) {
+    return Responses._400({ err });
+  }
+};
