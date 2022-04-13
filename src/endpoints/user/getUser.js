234779a3f15e12a7bd8e37318ba459a7027b17d2
andremelinski <andremelinski29@gmail.com>
2022-04-12 23:27:19 -0300

    add dynamo

--- src/endpoints/user/getUser.js
+++ src/endpoints/user/getUser.js
@@ -2,19 +2,20 @@ require("dotenv").config()
 const Responses = require('../../common/API_Responses')
 const Dynamo = require('../../common/DynamoDB')
 
-const userTableName = process.env.USER_TABLE_NAME
+const USER_TABLE_NAME = process.env.USER_TABLE_NAME
 exports.handler = async (event) => {
     
     if(event.pathParameters || event.pathParameters.ID) return Responses._400({message: event.pathParameters})
 
     let {ID} = event.pathParameters;
-    console.log({userTableName, ID})
-    const user = await Dynamo.get(ID, userTableName).catch(err => {
+    console.log(`Table name ${USER_TABLE_NAME}, ID ${ID}`)
+
+    const user = await Dynamo.get(ID, USER_TABLE_NAME).catch(err => {
         console.log("error in Dynamo Get",err)
         return null
     });
 
-    if(!user) return Responses._400({message: {ID, userTableName}})
+    if(!user) return Responses._400({message: {ID, USER_TABLE_NAME}})
 
     return Responses._200({user})
 }
